05b1be5bff1a4774d63d1c2d92f4051327fceb82
Nimrodrive5 <marcko_r@hotmail.com>
2022-09-08 12:08:38 -0500

    con appendFileSync añade texto a un archivo ya creado

--- file1.js
+++ file1.js
@@ -1,3 +1,14 @@
 const fs = require ('fs')
 
-fs.writeFileSync('clase4.txt', 'Mi primera vez escribiendo en un archivo')
+//fs.writeFileSync('clase4.txt', 'Mi segunda vez escribiendo en un archivo') //esto crea el archivo clase4 haciendo node file1.js
+
+/*
+try {
+    let contenido = fs.readFileSync('clase4.txt') 
+    console.log(contenido, toString()) //toString es necesario para codificar el contenido
+} catch (err){
+    fs.writeFileSync('clase4.txt', 'Ahora si existe')
+}
+*/
+
+fs.appendFileSync ('Clase4.txt', 'texto nuevo agregado')
